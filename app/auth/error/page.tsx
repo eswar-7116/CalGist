799fb621b2dfacf92c57e2d4e9f4eebe9cc20640
eswar-7116 <eswardudi06@gmail.com>
2025-06-25 22:27:26 +0530

    Reformat using prettier

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -1,9 +1,9 @@
 export default function AuthErrorPage() {
-    return (
-        <main className="flex flex-1 flex-col items-center justify-center h-full">
-            <h1 className="text-4xl font-bold mb-4">Authentication Error!</h1>
-            <p>An <b>Unknown error</b> occurred while logging you in.</p>
-            <p>Please try again.</p>
-        </main>
-    )
+  return (
+    <main className="flex flex-1 flex-col items-center justify-center h-full">
+      <h1 className="text-4xl font-bold mb-4">Authentication Error!</h1>
+      <p>An error occurred while logging you in.</p>
+      <p>Please try again.</p>
+    </main>
+  );
 }
