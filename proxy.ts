8cd2c1684e1109bbe42987d0c583d45bbbdefd06
eswar-7116 <eswardudi06@gmail.com>
2025-12-14 15:58:00 +0530

    Update packages

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { type NextRequest } from 'next/server'
 import { updateSession } from '@/lib/supabase/middleware'
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   console.log(` ${request.method} ${request.nextUrl.pathname}`)
   return await updateSession(request)
 }
